a055356b8dfb4154001e5723489064fbdc561e6b
Ricardo Andrés Marín Pinilla <ricardomarin0258@gmail.com>
2025-07-23 22:03:56 -0500

    test integración

--- src/main/java/com/admincore/microservice/product/model/Product.java
+++ src/main/java/com/admincore/microservice/product/model/Product.java
@@ -3,10 +3,12 @@ package com.admincore.microservice.product.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Entity
 @Table(name = "products")
